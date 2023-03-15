a69461539881a37ae13d62311db82cb1741ed002
Idle <651462+reonZ@users.noreply.github.com>
2023-03-15 23:07:12 +0100

    prevent system daily prep from deleting temp items

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { parseCustomDailies } from './dailies'
 import { restForTheNight } from './rest'
 import { renderCharacterSheetPF2e } from './sheet'
 import { DailyCustoms } from '@apps/customs'
+import { registerWrapper } from '@utils/libwrapper'
+import { warn } from '@utils/foundry/notification'
 
 export const MODULE_ID = 'pf2e-dailies'
 setModuleID(MODULE_ID)
@@ -37,4 +39,46 @@ Hooks.once('setup', () => {
 
 Hooks.once('ready', async () => {
     await parseCustomDailies()
+
+    if (!game.modules.get('lib-wrapper')?.active && game.user.isGM) {
+        warn('error.noLibwrapper', true)
+        return
+    }
+
+    registerWrapper(
+        'CONFIG.PF2E.Actor.documentClasses.character.prototype.performDailyCrafting',
+        onPerformDailyCrafting,
+        'OVERRIDE'
+    )
 })
+
+async function onPerformDailyCrafting(this: CharacterPF2e) {
+    const entries = (await this.getCraftingEntries()).filter(e => e.isDailyPrep)
+    const alchemicalEntries = entries.filter(e => e.isAlchemical)
+    const reagentCost = alchemicalEntries.reduce((sum, entry) => sum + entry.reagentCost, 0)
+    const reagentValue = (this.system.resources.crafting.infusedReagents.value || 0) - reagentCost
+
+    if (reagentValue < 0) {
+        ui.notifications.warn(game.i18n.localize('PF2E.CraftingTab.Alerts.MissingReagents'))
+        return
+    } else {
+        await this.update({ 'system.resources.crafting.infusedReagents.value': reagentValue })
+    }
+
+    for (const entry of entries) {
+        for (const formula of entry.preparedCraftingFormulas) {
+            const itemSource: PhysicalItemSource = formula.item.toObject()
+            itemSource.system.quantity = formula.quantity
+            itemSource.system.temporary = true
+            itemSource.system.size = this.ancestry?.size === 'tiny' ? 'tiny' : 'med'
+
+            if (
+                entry.isAlchemical &&
+                (itemSource.type === 'consumable' || itemSource.type === 'weapon' || itemSource.type === 'equipment')
+            ) {
+                itemSource.system.traits.value.push('infused')
+            }
+            await this.addToInventory(itemSource)
+        }
+    }
+}

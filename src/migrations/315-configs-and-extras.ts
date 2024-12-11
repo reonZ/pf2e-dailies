6fa97581bbf4f9e32261d98f0ac042f17cd9c55a
Idle <651462+reonZ@users.noreply.github.com>
2024-12-11 14:23:02 +0100

    migrate extra flags to use daily key context

---
+++ src/migrations/315-configs-and-extras.ts
@@ -0,0 +1,40 @@
+import { ModuleMigration } from "module-helpers/dist/migration";
+import { createMigrateActorFlag } from "./base";
+import { R } from "module-helpers";
+
+export default {
+    version: 3.15,
+    migrateActor: createMigrateActorFlag((actorFlag) => {
+        let updated = false;
+
+        for (const key of ["familiar", "animist"]) {
+            if (key in actorFlag) {
+                updated = true;
+                actorFlag[`-=${key}`] = true;
+                foundry.utils.setProperty(actorFlag, `config.dailies.${key}`, actorFlag[key]);
+            }
+        }
+
+        if ("extra" in actorFlag) {
+            const customs = R.omit(actorFlag.extra, ["staffData"]);
+            const customKeys = R.keys(customs);
+
+            if (customKeys.length) {
+                updated = true;
+                actorFlag.extra.custom = customs;
+
+                for (const key of customKeys) {
+                    actorFlag.extra[`-=${key}`] = true;
+                }
+            }
+
+            if ("staffData" in actorFlag.extra) {
+                updated = true;
+                actorFlag.extra["-=staffData"] = true;
+                actorFlag.extra["dailies.staves"] = actorFlag.extra.staffData;
+            }
+        }
+
+        return updated;
+    }),
+} satisfies ModuleMigration;
--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,2 +1,2 @@
 export { default as migration300 } from "./300-reset-dailies";
-export { default as migration315 } from "./315-configs";
+export { default as migration315 } from "./315-configs-and-extras";

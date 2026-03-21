6346d72beafbba4cf2e61e43285b3645f12606d0
Idle <651462+reonZ@users.noreply.github.com>
2026-03-21 17:45:34 +0100

    fix support sf2e for core-memories not actually working - fixes #155

--- src/data/core-memories.ts
+++ src/data/core-memories.ts
@@ -2,10 +2,17 @@ import { createDaily } from "daily";
 import { ChoiceSetRuleElement, FeatPF2e, ItemUUID, R, SkillSlug, SYSTEM } from "foundry-helpers";
 import { utils } from "utils";
 
-const ASSURANCE_UUID = "Compendium.pf2e.feats-srd.Item.W6Gl9ePmItfDHji0";
-const AUTOMATIC_KNOWLEDGE_UUID = "Compendium.pf2e.feats-srd.Item.H3I2X0f7v4EzwxuN";
+const ASSURANCE_UUID = SYSTEM.uuid(
+    "Compendium.pf2e.feats-srd.Item.W6Gl9ePmItfDHji0",
+    "Compendium.sf2e.feats.Item.W6Gl9ePmItfDHji0",
+);
 
-const RK_SKILLS = ["arcana", "crafting", "nature", "occultism", "religion", "society"] as const;
+const AUTOMATIC_KNOWLEDGE_UUID = SYSTEM.uuid(
+    "Compendium.pf2e.feats-srd.Item.H3I2X0f7v4EzwxuN",
+    "Compendium.sf2e.feats.Item.H3I2X0f7v4EzwxuN",
+);
+
+const RK_SKILLS = ["arcana", "computers", "crafting", "nature", "occultism", "religion", "society"] as SkillSlug[];
 
 const coreMemories = createDaily({
     key: "core-memories",
@@ -33,9 +40,11 @@ const coreMemories = createDaily({
         ];
     },
     process: async ({ actor, items, messages, rows, addFeat }) => {
+        const assuranceUUID = ASSURANCE_UUID();
+        const automaticKnowledgeUUID = AUTOMATIC_KNOWLEDGE_UUID();
         const slug = rows.skill as SkillSlug;
         const skill = actor.skills[slug];
-        const assurance = (await fromUuid<FeatPF2e>(ASSURANCE_UUID))?.toObject();
+        const assurance = (await fromUuid<FeatPF2e>(assuranceUUID))?.toObject();
         if (!assurance) return;
 
         const rule = (assurance.system.rules as ChoiceSetRuleElement[]).find((rule) => rule.flag === "assurance");
@@ -45,10 +54,10 @@ const coreMemories = createDaily({
         }
 
         addFeat(assurance, items.core);
-        messages.add("memories", { sourceId: ASSURANCE_UUID });
+        messages.add("memories", { sourceId: assuranceUUID });
 
         if (skill.rank > 1 && R.isIncludedIn(slug, RK_SKILLS)) {
-            const knowledge = (await fromUuid<FeatPF2e>(AUTOMATIC_KNOWLEDGE_UUID))?.toObject();
+            const knowledge = (await fromUuid<FeatPF2e>(automaticKnowledgeUUID))?.toObject();
 
             if (knowledge) {
                 const label = game.i18n.localize(CONFIG.PF2E.skills[slug].label);
@@ -57,7 +66,7 @@ const coreMemories = createDaily({
                 knowledge.system.actionType.value = "free";
 
                 addFeat(knowledge, items.core);
-                messages.add("memories", { sourceId: AUTOMATIC_KNOWLEDGE_UUID });
+                messages.add("memories", { sourceId: automaticKnowledgeUUID });
             }
         }
     },
